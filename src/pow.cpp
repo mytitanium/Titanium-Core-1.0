50492c309361eb32b01c33040b3034f0d9fcb696
sushilraghava <68848522+sushilraghava@users.noreply.github.com>
2021-01-04 00:41:07 +0600

    Update pow.cpp

--- src/pow.cpp
+++ src/pow.cpp
@@ -82,13 +82,13 @@ unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const Conse
 unsigned int static DarkGravityWave(const CBlockIndex* pindexLast, const Consensus::Params& params, const CBlockHeader *pblock) {
     /* current difficulty formula, ttm - DarkGravity v3, written by Evan Duffield - evan@ttm.org */
     const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
-    if (pindexLast->nHeight >= 53874) {
+    if (pindexLast->nHeight >= 1609698676) {
     	const arith_uint256 bnPowLimit = UintToArith256(params.kawpowLimit);
     }
     int64_t nPastBlocks = 24;
 
     // make sure we have at least (nPastBlocks + 1) blocks, otherwise just return powLimit
-    if (!pindexLast || pindexLast->nHeight < nPastBlocks || (pindexLast->nHeight >= 53874 && pindexLast->nHeight < 53898)) {
+    if (!pindexLast || pindexLast->nHeight < nPastBlocks ) {
         return bnPowLimit.GetCompact();
     }
 
