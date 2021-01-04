9e21fc9695771aa68e5f2c4cc4a5c316810b20c8
sushilraghava <68848522+sushilraghava@users.noreply.github.com>
2021-01-04 23:50:15 +0600

    Update block.cpp

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -48,11 +48,12 @@ uint256 CBlockHeader::GetKAWPOWHeaderHash() const
 std::string CBlock::ToString() const
 {
     std::stringstream s;
-    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, nNonce64=%u, vtx=%u)\n",
+    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, mix_hash=%s, nTime=%u, nBits=%08x, nNonce=%u, nNonce64=%u, vtx=%u)\n",
         GetHash().ToString(),
         nVersion,
         hashPrevBlock.ToString(),
         hashMerkleRoot.ToString(),
+	mix_hash.ToString(),
         nTime, nBits, nNonce,nNonce64,
         vtx.size());
     for (const auto& tx : vtx) {
