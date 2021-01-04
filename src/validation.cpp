0a69d526462f99ee10fd74de2bd44818dfbbd4bb
sushilraghava <68848522+sushilraghava@users.noreply.github.com>
2021-01-05 01:36:35 +0600

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -3266,15 +3266,16 @@ static bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationSta
 
     // Check proof of work
     const Consensus::Params& consensusParams = params.GetConsensus();
-    if(Params().NetworkIDString() == CBaseChainParams::MAIN && nHeight < 53875 ){
+    if(Params().NetworkIDString() == CBaseChainParams::MAIN ){
         // architecture issues with DGW v1 and v2)
         unsigned int nBitsNext = GetNextWorkRequired(pindexPrev, &block, consensusParams);
         double n1 = ConvertBitsToDouble(block.nBits);
         double n2 = ConvertBitsToDouble(nBitsNext);
 
-        if (abs(n1-n2) > n1*0.5)
+        if (abs(n1-n2) > n1*0.5 && nHeight < 53875)
             return state.DoS(100, error("%s : incorrect proof of work (DGW pre-fork) - %f %f %f at %d", __func__, abs(n1-n2), n1, n2, nHeight),
                             REJECT_INVALID, "bad-diffbits");
+        
     } else {
         if (block.nBits != GetNextWorkRequired(pindexPrev, &block, consensusParams))
             return state.DoS(100, false, REJECT_INVALID, "bad-diffbits", false, strprintf("incorrect proof of work at %d", nHeight));
